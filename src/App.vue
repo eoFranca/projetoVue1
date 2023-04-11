4577c6a4ff356a34701d04f8094dc01455cb7f98
eoFranca <erfranca.com@gmail.com>
2023-04-11 11:52:24 -0300

    --

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container ">
+
+    <div class="form x">
+      <label for="nome">Nome</label>  <br> 
+<input type="text"><br>
+
+
+<label for="email">email</label><br>
+<input type="email" name="a" id="email"><br>
+<label for="password">senha</label><br>
+<input type="password" name="" id=""><br>
+<label for="password">confirmar senha</label><br>
+<input type="password" name="" id=""><br>
+  <label for="">Data de Nascimento</label><br>
+  <input type="date" name="" id=""><br>
+  
+  <label for="Endereço">Endereço</label><br>
+  <input type="text" name="" id="">
+  <br>
+  <label for="">Estado
+  </label>
+  <br>
+  <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+    <option value="">Selecione seu Estado</option>
+    <option value="AC">AC</option> 
+    <option value="AL">AL</option> 
+    <option value="AM">AM.container-fluid">ES</option> 
+        <option value="GO">GO</option> 
+        <option value="MA">MA</option> 
+        <option value="MT">MT</option> 
+        <option value="MS">MS</option> 
+        <option value="MG">MG</option> 
+        <option value="PA">PA</option> 
+        <option value="PB">PB</option> 
+        <option value="PR">PR</option> 
+        <option value="PE">PE</option> 
+        <option value="PI">PI</option> 
+        <option value="RJ">RJ</option> 
+        <option value="RN">RN</option> 
+        <option value="RO">RO</option> 
+        <option value="RS">RS</option> 
+        <option value="RR">RO</option> 
+        <option value="SC">SC</option> 
+        <option value="SE">SE</option> 
+        <option value="SP">SP</option> 
+        <option value="TO">TO</option> 
+      </select>
+      <label for="">Cidade</label><br>
+      <input type="text"><br>
+      <label for="">Hobbies</label><br>
+      <input type="text" name="" id=""><br>
+      <label for="">Linguagens de programação</label><br>
+      <input type="text"> <br>
+      <label for="">Biografia</label><br>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <br><br>
+      <button class="btn btn-primary">Cadastrar-se</button>
+      
+    </div>
+  </div>
+  </template>
+
+<style scoped>
+
+
+</style>
